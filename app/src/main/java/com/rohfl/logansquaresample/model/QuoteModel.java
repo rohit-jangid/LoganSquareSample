b0f9b67312af0bbb2590495543a730679e228874
Rohit Jangid <rohit.jangid@internshala.com>
2022-01-07 14:40:55 +0530

    Add model.

---
+++ app/src/main/java/com/rohfl/logansquaresample/model/QuoteModel.java
@@ -0,0 +1,96 @@
+package com.rohfl.logansquaresample.model;
+
+import com.bluelinelabs.logansquare.annotation.JsonField;
+import com.bluelinelabs.logansquare.annotation.JsonObject;
+
+@JsonObject
+public class QuoteModel {
+
+    @JsonField(name = "_id")
+    private String id;
+
+    @JsonField
+    private String[] tags;
+
+    @JsonField
+    private String content;
+
+    @JsonField
+    private String author;
+
+    @JsonField
+    private String authorSlug;
+
+    @JsonField
+    private int length;
+
+    @JsonField
+    private String dateAdded;
+
+    @JsonField
+    private String dateModified;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String[] getTags() {
+        return tags;
+    }
+
+    public void setTags(String[] tags) {
+        this.tags = tags;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getAuthorSlug() {
+        return authorSlug;
+    }
+
+    public void setAuthorSlug(String authorSlug) {
+        this.authorSlug = authorSlug;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public String getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(String dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public String getDateModified() {
+        return dateModified;
+    }
+
+    public void setDateModified(String dateModified) {
+        this.dateModified = dateModified;
+    }
+}
